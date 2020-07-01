6d6162c1ecf8e5a51e79a71ada88506cbc568baf
widiresa <67679995+widiresa@users.noreply.github.com>
2020-07-01 14:47:42 +0700

    web

--- UAS/myadmin/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "labportal";
-
-    $koneksi = mysqli_connect($server,$username,$password,$database) OR DIE ("Koneksi Gagal");
-?>
